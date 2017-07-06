952778c9fd3d4d952238ee6541394c6182b24e47
leegroup <maxwass@seas.upenn.edu>
2017-07-05 22:41:41 -0400

    changed waypointGenerator class. Bugs before were due to misunderstanding of Matlab classes. If a method changes the class variables, the method must return a reference to the new object, this is why you see most methods return 'obj'

--- waypointGenerator.m
+++ waypointGenerator.m
@@ -3,8 +3,8 @@ classdef waypointGenerator
         %path = [ 43.12342, 76.234234; 45.345345,65.34535; .... ]
         %nextPointer = index in path of waypoint we are currently going to
     %Its methods return the updated waypoint to go to
-    properties
-        path
+    properties (SetAccess = private)
+        path = []
         nextPointer
         noPath
     end
@@ -19,32 +19,31 @@ classdef waypointGenerator
             end
             obj.nextPointer = 1;
         end
-%         function obj = waypointGenerator()
-%             obj.noPath = true;
-%         end
         
-        
-        function nextWaypoint = getNextWaypoint(this, currentGPS)
-            if (this.noPath == true)
-               disp("WayPoint Generator: No Path Entered!!")
+        function [obj, nextWaypoint] = getNextWaypoint(obj, currentGPS)
+            if (obj.noPath == true)
+               %disp("WayPoint Generator: No Path Entered!!")
                nextWaypoint = NaN;
                return;
            end
             
             %reached next waypoint?
-            if this.reachedWaypoint(this.path(this.nextPointer, :), currentGPS, 1)
+            next_waypoint = obj.path(obj.nextPointer, :);
+            reached_waypoint = obj.reachedWaypoint(currentGPS, next_waypoint, 1);
+            
+            if reached_waypoint
                 %if yes increment nextPointer as long as not at end of path
-               if(objnextPointer < this.path.length)
-                   this.nextPointer = this.nextPointer + 1; 
+               if(obj.nextPointer < size(obj.path,1))
+                   obj.nextPointer = (obj.nextPointer + 1); 
                else
                    %TRIGGER END OF PATH
                end   
             end
-            nextWaypoint = this.path(this.nextPointer,:);
+            nextWaypoint = obj.path(obj.nextPointer,:);
         end
         
         function b = reachedWaypoint(this, currentGPS, nextWaypointGPS, distanceThreshold)
-           %% GPS comes in as [long, lattitude]
+           % GPS comes in as [long, lattitude]
            
            if (this.noPath == true)
                disp("WayPoint Generator: No Path Entered!!")
@@ -54,10 +53,12 @@ classdef waypointGenerator
            
            
            %%if we are within threshold (m) then increment pointer
+           %%SAME INPUT NOT GIVING ZZERO DISTANCE OUPUTS IN EITHER DIST
+           %%FUNCTION
            
            %needs to be tested! - flip input gps for correct long/lat order
-           %[distKM] = lldistkm(fliplr(currentGPS), fliplr(nextWaypointGPS));
-           %distM = distKM*1000;
+           [distKM] = lldistkm(fliplr(currentGPS), fliplr(nextWaypointGPS));
+           distMP = distKM*1000;
            
            xy_waypoint = llToMeters(nextWaypointGPS(1),nextWaypointGPS(2));
            xy_jackal = llToMeters(currentGPS(1), currentGPS(2));
@@ -73,9 +74,18 @@ classdef waypointGenerator
           
         end
 
-        function changePath(this, newPath)
-            this.path = newPath;
-            this.nextPointer = 1;
+        function obj = setPath(obj, newPath)
+            obj.path = newPath;
+            obj.nextPointer = 1;
+            obj.noPath = false;
+        end
+        
+        function obj = setNextPointer(obj, index)
+            obj.nextPointer = index;
+        end
+        
+        function i = getNextPointer(obj)
+            i = obj.nextPointer;
         end
         
     end
